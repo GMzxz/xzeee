b3104b0490166afcdb87200715e0415bc1bdeb40
GMzxz <93560655+GMzxz@users.noreply.github.com>
2021-12-26 13:28:11 +0700

    Add files via upload

---
+++ INTERNET.php
@@ -0,0 +1,23 @@
+{"Version":"1.2",
+"Url":"https://github.com/GMzxz/xzeee/blob/main/INTERNETVPNFREE.zip" ,
+"Changelog":"CatVPN
+รายละเอียดภายในแอป
+Free-SG_TRUE&DTAC&AIS
+Free-US_TRUE&DTAC&AIS
+ประเทศของเซิร์ฟเวอร์
+SG 5 เซิร์ฟเวอร์ 
+US 5 เซิร์ฟเวอร์
+การอัปเดตต้องมีอินเตอร์เน็ตทุกครั้ง",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
